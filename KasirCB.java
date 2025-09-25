2516295aa42ab691c4aa455d04bffe241ee71471
Muhammad Hikmal Al-Ghifary <mhikmal.ag@gmail.com>
2025-09-25 07:43:12 +0700

    Template awal

---
+++ KasirCB.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class KasirCB{
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        int pilih, total = 0, totMakan, totMinum;
+
+        System.out.println("========= Warung Makan =========");
+        System.out.println("1. Nasi Goreng Spesial Rp 15.000");
+        System.out.println("2. Ayam Bakar Madu Rp 25.000");
+        System.out.println("3. Soto Ayam Lamongan Rp 12.000");
+        System.out.println("4. Es Teh Manis Rp 5.000");
+        System.out.println("5. Es Jeruk Rp. 7.000");
+        System.out.println("----------------------------------");
+        System.out.println("Masukkan pilihan: ");
+        // pilih
+        System.out.print("Masukkan jumlah : ");
+        // jumlah
+        System.out.print("Pesanan lain (Y/T) :");
+        // pilih
+        
+    }
+}
